c22e4c3e9de3ed594d16b608d0c2b9230cb8c928
Carlos Noel <carlosnoelg3@gmail.com>
2018-09-28 10:35:19 -0500

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+// ell PROCESS es una variable que siempre esta corriendo
+
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
