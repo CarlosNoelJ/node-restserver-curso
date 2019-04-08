a9d00b39c662f7ad1a75eb8318dde8054cf7e9d9
Carlos Johnny Noel Bernaola <carlosnoelg3@outlook.com>
2019-04-08 08:52:33 -0500

    protecting uriDB

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Ice-Blck:Ydaleu92.@cluster0-zjjp9.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
